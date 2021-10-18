caeed53080ba5d5855c34ec851e6ae9cde608c25
CF <pclienth@hotmail.com>
2021-10-18 10:44:23 +0800

    Transaction Delete

--- resources/views/pages/transaction/index.blade.php
+++ resources/views/pages/transaction/index.blade.php
@@ -3,17 +3,19 @@
 
 @section('title', 'Support Tickets')
 
+@section('vendor-style')
+  <link rel="stylesheet" href="{{ asset(mix('vendors/css/animate/animate.min.css')) }}">
+  <link rel="stylesheet" href="{{ asset(mix('vendors/css/extensions/sweetalert2.min.css')) }}">
+@endsection
+
 @section('content')
 <!-- Basic Tables start -->
 <div class="row" id="basic-table">
   <div class="col-12">
     <div class="card">
-      {{-- <div class="card-header">
-        <h4 class="card-title">Table Basic</h4>
+      <div class="card-header">
+        <h4 class="card-title">Transactions</h4>
       </div>
-      <div class="card-body">
-        <p class="card-text"></p>
-      </div> --}}
       <div class="table-responsive">
         <table class="table">
           <thead>
@@ -33,9 +35,13 @@
                 <td>{{ $e->status }}</td>
                 <td>{{ $e->created_at }}</td>
                 <td>
-                  <a class="btn btn-icon btn-danger">
+                  <a class="btn btn-icon btn-danger" onclick="onDelete(this)">
                     <i data-feather="trash"></i>
                   </a>
+                  <form action="{{ route('transactions.destroy', $e->id) }}" class="delete-form" method="POST" style="display:none">
+                    <input type="hidden" name="_method" value="DELETE">
+                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                  </form>
                 </td>
               </tr>
             @endforeach
@@ -47,4 +53,31 @@
   </div>
 </div>
 <!-- Basic Tables end -->
+@endsection
+@section('vendor-script')
+  <script src="{{ asset(mix('vendors/js/extensions/sweetalert2.all.min.js')) }}"></script>
+  <script src="{{ asset(mix('vendors/js/extensions/polyfill.min.js')) }}"></script>
+@endsection
+@section('page-script')
+<script>
+  async function onDelete(obj) {
+    var delete_form = $(obj).closest('.td-actions').children('.delete-form')
+    var swal_result = await Swal.fire({
+      title: 'Warning!',
+      text: 'Are you sure to delete?',
+      icon: 'warning',
+      customClass: {
+        confirmButton: 'btn btn-primary',
+        cancelButton: 'btn btn-outline-danger ms-1'
+      },
+      showCancelButton: true,
+      confirmButtonText: 'OK',
+      cancelButtonText: 'Cancel',
+      buttonsStyling: false
+    });
+    if (swal_result.isConfirmed) {
+      delete_form.submit();
+    }
+  }
+</script>
 @endsection
