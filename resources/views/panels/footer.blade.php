7ef6b5f8b2ce119f4e378c84c81e2b11a30dcac4
CF <pclienth@hotmail.com>
2021-11-04 06:27:59 +0800

    Copyright text on Footer

--- resources/views/panels/footer.blade.php
+++ resources/views/panels/footer.blade.php
@@ -2,7 +2,7 @@
 <footer class="footer footer-light {{($configData['footerType'] === 'footer-hidden') ? 'd-none':''}} {{$configData['footerType']}}">
   <p class="clearfix mb-0">
     <span class="float-md-start d-block d-md-inline-block mt-25">
-      {{ $company->copy_right_text }}
+      {!! $company->copy_right_text !!}
     </span>
   </p>
 </footer>
