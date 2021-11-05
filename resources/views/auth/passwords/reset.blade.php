2e58c06e3480293797b88706ca1f9f8d46c476ec
CF <pclienth@hotmail.com>
2021-11-05 12:20:16 +0800

    reset password blade

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -63,53 +63,6 @@
     </div>
 </div>
 @endsection --}}
-{{-- @extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection --}}
 @php
   $configData = Helper::applClasses();
 @endphp
@@ -169,13 +122,16 @@
         </div>
         <h2 class="card-title fw-bold mb-1">Reset Password 🔒</h2>
         <p class="card-text mb-2">Your new password must be different from previously used passwords</p>
-        <form class="auth-reset-password-form mt-2" action="/auth/login-cover" method="GET">
+        <form class="auth-reset-password-form mt-2" action="{{route('password.update')}}" method="GET">
+          @csrf
+          <input type="hidden" name="token" value="{{ $token }}">
+          <input id="email" type="hidden" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
           <div class="mb-1">
             <div class="d-flex justify-content-between">
               <label class="form-label" for="reset-password-new">New Password</label>
             </div>
             <div class="input-group input-group-merge form-password-toggle">
-              <input class="form-control form-control-merge" id="reset-password-new" type="password" name="reset-password-new" placeholder="············" aria-describedby="reset-password-new" autofocus="" tabindex="1" />
+              <input class="form-control form-control-merge" id="reset-password-new" type="password" name="password" placeholder="············" aria-describedby="reset-password-new" autofocus="" tabindex="1" />
               <span class="input-group-text cursor-pointer"><i data-feather="eye"></i></span>
             </div>
           </div>
@@ -184,14 +140,14 @@
               <label class="form-label" for="reset-password-confirm">Confirm Password</label>
             </div>
             <div class="input-group input-group-merge form-password-toggle">
-              <input class="form-control form-control-merge" id="reset-password-confirm" type="password" name="reset-password-confirm" placeholder="············" aria-describedby="reset-password-confirm" tabindex="2" />
+              <input class="form-control form-control-merge" id="reset-password-confirm" type="password" name="password_confirmation" placeholder="············" aria-describedby="reset-password-confirm" tabindex="2" />
               <span class="input-group-text cursor-pointer"><i data-feather="eye"></i></span>
             </div>
           </div>
           <button class="btn btn-primary w-100" tabindex="3">Set New Password</button>
         </form>
         <p class="text-center mt-2">
-          <a href="{{url('auth/login-cover')}}">
+          <a href="{{url('login')}}">
             <i data-feather="chevron-left"></i> Back to login
           </a>
         </p>
