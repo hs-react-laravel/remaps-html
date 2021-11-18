6c8dc67bd391aff61ccec1177bf37ce3f7034584
CF <pclienth@hotmail.com>
2021-11-18 12:59:51 +0800

    Buy Credit Fix

--- app/Http/Controllers/Consumer/BuyTuningCreditsController.php
+++ app/Http/Controllers/Consumer/BuyTuningCreditsController.php
@@ -17,10 +17,10 @@ class BuyTuningCreditsController extends MasterController
 {
     protected $paypal_client;
     public function __construct() {
+        parent::__construct();
         $user = Auth::guard('customer')->user();
         $env = new ProductionEnvironment($user->company->paypal_client_id, $user->company->paypal_secret);
         $this->paypal_client = new PayPalHttpClient($env);
-        parent::__construct();
     }
     public function index()
     {
