0f0137f1a40539250b492d8e519f49c8a514ac94
CF <pclienth@hotmail.com>
2021-12-01 03:33:07 +0800

    Invoice Pdf Image

--- resources/views/pdf/invoice.blade.php
+++ resources/views/pdf/invoice.blade.php
@@ -13,7 +13,7 @@
           <td valign="top">
 
             @if(\File::exists('storage/uploads/logo/'.$company->logo))
-              <img src="{{ storage_path('app/public/uploads/logo/'.$company->logo) }}" alt="" style="width:280px" />
+              <img src="{{ public_path('storage/uploads/logo/'.$company->logo) }}" alt="" style="width:280px" />
             @endif
           </td>
           <td align="top"><table width="100%" border="0" cellspacing="0" cellpadding="0">
