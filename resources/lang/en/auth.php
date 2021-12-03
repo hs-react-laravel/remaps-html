1e45b991c1d8a49ed243abf6286e3f7ae97be441
CF <pclienth@hotmail.com>
2021-12-03 10:38:18 +0800

    Locale Auth Strings

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -15,5 +15,10 @@ return [
 
     'failed' => 'These credentials do not match our records.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
-
+    'failed_inactive' => 'Your account is\'t active yet. Contact with administrator.',
+    'invalid_admin_privilege' => 'You haven\'t right privilege to access admin panel.',
+    'invalid_customer_privilege' => 'You haven\'t right privilege to access customer panel directly.',
+    'logged_out' => 'You have been successfully logged out!',
+    'password_changed' => 'Your password has been changed.',
+    'invalid_domain' => 'We can\'t find a user with that e-mail address.',
 ];
