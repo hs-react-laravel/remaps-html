09f0ed3969f4da95138c4ba03c11ef316fce8557
CF <pclienth@hotmail.com>
2022-07-25 10:47:58 -0400

    Email template test

--- app/Mail/TicketFileCreated.php
+++ app/Mail/TicketFileCreated.php
@@ -47,8 +47,8 @@ class TicketFileCreated extends Mailable
 
 			$subject = str_replace('##CUSTOMER_NAME', ucwords($this->user->first_name.' '.$this->user->last_name), $subject);
 
-            $body = str_replace('##APP_NAME', $this->user->name, $body);
-            $body = str_replace('##APP_LOGO', $this->user->company->logo, $body);
+            $body = str_replace('##APP_NAME', $this->user->company->name, $body);
+            $body = str_replace('##APP_LOGO', asset('uploads/logo/'. $this->user->company->logo), $body);
 			$body = str_replace('##CUSTOMER_NAME', ucwords($this->user->first_name.' '.$this->user->last_name), $body);
 			$body = str_replace('##MESSAGE', ucfirst($this->jobDetail), $body);
             $this->subject($subject)
