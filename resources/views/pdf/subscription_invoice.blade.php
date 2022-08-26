7445c7514ceb21cc0042a85bfd44a6cbc4c25765
CF <pclienth@hotmail.com>
2022-08-26 10:50:16 -0400

    Logo image on subscription invoice

--- resources/views/pdf/subscription_invoice.blade.php
+++ resources/views/pdf/subscription_invoice.blade.php
@@ -14,6 +14,7 @@
             @if(\File::exists('storage/uploads/logo/'.$company->logo))
               <img src="{{ asset('storage/uploads/logo/'.$company->logo) }}" alt="" style="width:280px" />
             @endif
+            {{asset('storage/uploads/logo/'.$company->logo)}}
           </td>
           <td align="top"><table width="100%" border="0" cellspacing="0" cellpadding="0">
             <tr>
