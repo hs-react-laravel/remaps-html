9ef1cc64dc288181f8e9b370a745f83965ed35dc
CF <pclienth@hotmail.com>
2022-10-06 12:07:24 -0400

    Exception process for stripe payment

--- app/Http/Controllers/Consumer/BuyTuningCreditsController.php
+++ app/Http/Controllers/Consumer/BuyTuningCreditsController.php
@@ -198,94 +198,98 @@ class BuyTuningCreditsController extends MasterController
     }
 
     public function stripePost(Request $request) {
-        $tuningCreditGroup = TuningCreditGroup::find($request->group_id);
-        $groupCreditTires  = $this->user->tuningCreditGroup->tuningCreditTires()->withPivot('from_credit', 'for_credit')->wherePivot('from_credit', '!=', 0.00);
-        if (!$groupCreditTires) {
-            $groupCreditTires  = $this->user->tuningEVCCreditGroup->tuningCreditTires()->withPivot('from_credit', 'for_credit')->wherePivot('from_credit', '!=', 0.00);
-        }
-        $tire = $groupCreditTires->where('id', $request->tire_id)->first();
-
-        $isVatCalculation = ($this->company->vat_number != null) && ($this->company->vat_percentage != null) && ($this->user->add_tax);
-        $vat_percentage = $isVatCalculation ? $this->company->vat_percentage : 0;
-        $tax = $tire->pivot->for_credit * $vat_percentage / 100;
-
-        $total_amount = $tire->pivot->for_credit + $tax;
-
-        $stripe = new StripeClient($this->user->company->stripe_secret);
-        // $stripe = new StripeClient("sk_test_Woo4C4CI3cf0dvngmUuty49A00csvMGrNg");
-
-        $result = $stripe->charges->create([
-            'amount' => $total_amount * 100,
-            'currency' => $this->company->paypal_currency_code,
-            'source' => $request->stripeToken,
-            'description' => $tuningCreditGroup->name.'('.$tire->amount.' credits)'
-        ]);
-        $user = $this->user;
-        if ($request->stripe_credit_type == 'normal') {
-            $totalCredits = ($user->tuning_credits + $tire->amount);
-            $user->tuning_credits = $totalCredits;
-            $user->save();
-        } else {
-            $url = "https://evc.de/services/api_resellercredits.asp";
-            $dataArray = array(
-                'apiid'=>'j34sbc93hb90',
-                'username'=> $user->company->reseller_id,
-                'password'=> $user->company->reseller_password,
-                'verb'=>'addcustomeraccount',
-                'customer' => $user->reseller_id,
-                'credits' => $tire->amount
-            );
-            $ch = curl_init();
-            $data = http_build_query($dataArray);
-            $getUrl = $url."?".$data;
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-            curl_setopt($ch, CURLOPT_URL, $getUrl);
-            curl_setopt($ch, CURLOPT_TIMEOUT, 500);
-
-            $response = curl_exec($ch);
-            if (strpos($response, 'ok') !== FALSE) {
-                // \Alert::success(__('customer.payment_success'))->flash();
+        try {
+            $tuningCreditGroup = TuningCreditGroup::find($request->group_id);
+            $groupCreditTires  = $this->user->tuningCreditGroup->tuningCreditTires()->withPivot('from_credit', 'for_credit')->wherePivot('from_credit', '!=', 0.00);
+            if (!$groupCreditTires) {
+                $groupCreditTires  = $this->user->tuningEVCCreditGroup->tuningCreditTires()->withPivot('from_credit', 'for_credit')->wherePivot('from_credit', '!=', 0.00);
             }
+            $tire = $groupCreditTires->where('id', $request->tire_id)->first();
+
+            $isVatCalculation = ($this->company->vat_number != null) && ($this->company->vat_percentage != null) && ($this->user->add_tax);
+            $vat_percentage = $isVatCalculation ? $this->company->vat_percentage : 0;
+            $tax = $tire->pivot->for_credit * $vat_percentage / 100;
+
+            $total_amount = $tire->pivot->for_credit + $tax;
+
+            $stripe = new StripeClient($this->user->company->stripe_secret);
+            // $stripe = new StripeClient("sk_test_Woo4C4CI3cf0dvngmUuty49A00csvMGrNg");
+
+            $result = $stripe->charges->create([
+                'amount' => $total_amount * 100,
+                'currency' => $this->company->paypal_currency_code,
+                'source' => $request->stripeToken,
+                'description' => $tuningCreditGroup->name.'('.$tire->amount.' credits)'
+            ]);
+            $user = $this->user;
+            if ($request->stripe_credit_type == 'normal') {
+                $totalCredits = ($user->tuning_credits + $tire->amount);
+                $user->tuning_credits = $totalCredits;
+                $user->save();
+            } else {
+                $url = "https://evc.de/services/api_resellercredits.asp";
+                $dataArray = array(
+                    'apiid'=>'j34sbc93hb90',
+                    'username'=> $user->company->reseller_id,
+                    'password'=> $user->company->reseller_password,
+                    'verb'=>'addcustomeraccount',
+                    'customer' => $user->reseller_id,
+                    'credits' => $tire->amount
+                );
+                $ch = curl_init();
+                $data = http_build_query($dataArray);
+                $getUrl = $url."?".$data;
+                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+                curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+                curl_setopt($ch, CURLOPT_URL, $getUrl);
+                curl_setopt($ch, CURLOPT_TIMEOUT, 500);
+
+                $response = curl_exec($ch);
+                if (strpos($response, 'ok') !== FALSE) {
+                    // \Alert::success(__('customer.payment_success'))->flash();
+                }
+            }
+            $displableId = \App\Models\Order::wherehas('user', function($query) use($user){
+                $query->where('company_id', $user->company_id);
+            })->max('displayable_id');
+
+            $displableId++;
+
+            /* save order */
+            $order = new \App\Models\Order();
+            $order->user_id = $this->user->id;
+            $order->transaction_id = $result->balance_transaction;
+            $order->invoice_id = $result->id;
+            $order->vat_number = $this->company->vat_number;
+            $order->vat_percentage = $this->company->vat_percentage;
+            $order->tax_amount = $tax;
+            $order->amount = $result->amount_captured / 100;
+            $order->description = $result->description;
+            $order->status = config('constants.order_status.completed');
+            $order->displayable_id = $displableId;
+            $order->save();
+
+            /* save transaction */
+            $transaction = new \App\Models\Transaction();
+            $transaction->user_id = $this->user->id;
+            $transaction->credits = number_format($tire->amount, 2);
+            if ($request->stripe_credit_type == 'normal') {
+                $transaction->description = "Tuning credits purchase";
+            } else {
+                $transaction->description = "EVC credits purchase";
+            }
+            $transaction->status = config('constants.transaction_status.completed');
+            $transaction->save();
+
+            if ($result->status == 'succeeded') {
+                // \Alert::success(__('customer.payment_success'))->flash();
+                return redirect(route('consumer.buy-credits'));
+            }
+            // \Alert::error(__('customer.payment_failed'))->flash();
+        }catch(\Exception $e){
+            session()->flash('error', $e->getMessage());
         }
-        $displableId = \App\Models\Order::wherehas('user', function($query) use($user){
-            $query->where('company_id', $user->company_id);
-        })->max('displayable_id');
-
-        $displableId++;
-
-        /* save order */
-        $order = new \App\Models\Order();
-        $order->user_id = $this->user->id;
-        $order->transaction_id = $result->balance_transaction;
-        $order->invoice_id = $result->id;
-        $order->vat_number = $this->company->vat_number;
-        $order->vat_percentage = $this->company->vat_percentage;
-        $order->tax_amount = $tax;
-        $order->amount = $result->amount_captured / 100;
-        $order->description = $result->description;
-        $order->status = config('constants.order_status.completed');
-        $order->displayable_id = $displableId;
-        $order->save();
-
-        /* save transaction */
-        $transaction = new \App\Models\Transaction();
-        $transaction->user_id = $this->user->id;
-        $transaction->credits = number_format($tire->amount, 2);
-        if ($request->stripe_credit_type == 'normal') {
-            $transaction->description = "Tuning credits purchase";
-        } else {
-            $transaction->description = "EVC credits purchase";
-        }
-        $transaction->status = config('constants.transaction_status.completed');
-        $transaction->save();
-
-        if ($result->status == 'succeeded') {
-            // \Alert::success(__('customer.payment_success'))->flash();
-            return redirect(route('consumer.buy-credits'));
-        }
-        // \Alert::error(__('customer.payment_failed'))->flash();
         return redirect(route('consumer.buy-credits'));
     }
 }
