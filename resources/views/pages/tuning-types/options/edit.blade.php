87e628de4f67dc46083ed9086e90827af806953d
CF <pclienth@hotmail.com>
2023-02-06 07:41:39 -0500

    Remove 01 step from tuning type option edit

--- resources/views/pages/tuning-types/options/edit.blade.php
+++ resources/views/pages/tuning-types/options/edit.blade.php
@@ -35,7 +35,7 @@
             <div class="row mb-1">
               <div class="col-xl-4 col-md-6 col-12">
                 <label class="form-label" for="credits">Credits</label>
-                <input type="number" class="form-control" id="credits" name="credits" value="{{ str_replace(“,”, “”, $entry->credits) }}" />
+                <input type="number" class="form-control" id="credits" name="credits" value="{{ str_replace(",", "", $entry->credits) }}" />
               </div>
             </div>
             <button type="submit" class="btn btn-primary me-1">Submit</button>
