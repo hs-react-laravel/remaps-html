4b9610edc2264130dc6ec6f84078dbc5819c3b0f
CF <pclienth@hotmail.com>
2023-05-24 09:34:50 -0400

    Use only default for password reset

--- app/Http/Controllers/Auth/Staff/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/Staff/ForgotPasswordController.php
@@ -30,29 +30,19 @@ class ForgotPasswordController extends Controller
         if (!$this->company){
             abort(400, 'No such domain('.url("").') is registerd with system. Please contact to webmaster.');
         }
-        if ($this->company->mail_host && $this->company->mail_port && $this->company->mail_encryption
-            && $this->company->mail_username && $this->company->mail_password) {
-            Config::set('mail.default', $this->company->mail_driver);
-            Config::set('mail.mailers.smtp.host', $this->company->mail_host);
-            Config::set('mail.mailers.smtp.port', $this->company->mail_port);
-            Config::set('mail.mailers.smtp.encryption', $this->company->mail_encryption);
-            Config::set('mail.mailers.smtp.username', $this->company->mail_username);
-            Config::set('mail.mailers.smtp.password', $this->company->mail_password);
-            Config::set('mail.from.address',$this->company->mail_username );
-        } else {
-            Config::set('mail.default', 'smtp');
-            Config::set('mail.mailers.smtp.host', 'mail.remapdash.com');
-            Config::set('mail.mailers.smtp.port', 25);
-            Config::set('mail.mailers.smtp.encryption', '');
-            Config::set('mail.mailers.smtp.username', 'no-reply@remapdash.com');
-            Config::set('mail.mailers.smtp.password', '73B#6lbt9');
-            Config::set('mail.from.address', 'no-reply@remapdash.com');
-        }
+        Config::set('mail.default', 'smtp');
+        Config::set('mail.mailers.smtp.host', 'mail.remapdash.com');
+        Config::set('mail.mailers.smtp.port', 25);
+        Config::set('mail.mailers.smtp.encryption', '');
+        Config::set('mail.mailers.smtp.username', 'passwrd-reset@remapdash.com');
+        Config::set('mail.mailers.smtp.password', '4s29ih0L&');
+        Config::set('mail.from.address', 'passwrd-reset@remapdash.com');
         Config::set('mail.from.name', $this->company->name);
         Config::set('app.name', $this->company->name);
         Config::set('app.url', $this->company->v2_domain_link);
         Config::set('app.logo', asset('storage/uploads/logo/'.$this->company->logo));
         view()->share('company', $this->company);
+        Log::info("Password Reset");
     }
     /**
      * Reset password broker.
