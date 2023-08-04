9795196a22623698606a94043c600293ff0df827
CF <pclienth@hotmail.com>
2023-08-03 22:34:12 -0400

    snippet test

--- resources/views/Frontend/home.blade.php
+++ resources/views/Frontend/home.blade.php
@@ -93,7 +93,7 @@
     </div>
 
     </section>
-    <script src="http://remapdash.com/api/snippetjs?id=12"></script>
+    <script src="https://remapdash.com/api/snippetjs?id=12"></script>
     <section id="howitworks" class="features features-section">
       <div class="container">
         <div class="section-header">
