a9da8bda86fc8d91e7cbb1009bb25464bb430a4f
CF <pclienth@hotmail.com>
2023-08-24 09:50:56 -0400

    API forgot password

--- app/Http/Controllers/Frontend/APIResetPasswordController.php
+++ app/Http/Controllers/Frontend/APIResetPasswordController.php
@@ -11,25 +11,6 @@ use View;
 
 class APIResetPasswordController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reset Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller is responsible for handling password reset requests
-    | and uses a simple trait to include this behavior. You're free to
-    | explore this trait and override any methods you wish to tweak.
-    |
-    */
-
-    use ResetsPasswords;
-
-    /**
-     * Where to redirect users after resetting their password.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/api-dashboard';
     protected $company;
 
     /**
@@ -39,20 +20,9 @@ class APIResetPasswordController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest:apiusers');
         $this->company = \App\Models\Company::where('is_default', 1)->first();
 
         view()->share('company', $this->company);
-    }
-
-    /**
-     * Reset the guard.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function guard()
-    {
-        return Auth::guard('apiusers');
     }
 
     /**
@@ -65,14 +35,5 @@ class APIResetPasswordController extends Controller
         return view('Frontend.api_forgot_reset')->with(
             ['token' => $token, 'email' => $request->email]
         );
-    }
-
-    /**
-     * Reset password broker.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function broker() {
-        return Password::broker('apiusers');
     }
 }
