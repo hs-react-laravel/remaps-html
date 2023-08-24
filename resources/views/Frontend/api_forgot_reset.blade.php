0e1465d6b2c13483238a69f4e134b8cd89e59e03
CF <pclienth@hotmail.com>
2023-08-24 10:05:42 -0400

    API forgot password

--- resources/views/Frontend/api_forgot_reset.blade.php
+++ resources/views/Frontend/api_forgot_reset.blade.php
@@ -85,11 +85,11 @@
                         <input type="hidden" name="token" value="{{ $token }}">
                         <div class="form-group">
                             {!! Form::label('password', 'Password') !!}
-                            {!! Form::password('password', '', ['class' => 'form-control', 'placeholder'=>'Password']) !!}
+                            {!! Form::password('password', ['class' => 'form-control', 'placeholder'=>'Password']) !!}
                         </div>
                         <div class="form-group">
                             {!! Form::label('password_confirmation', 'Password Confirmation') !!}
-                            {!! Form::password('password_confirmation', '', ['class' => 'form-control', 'placeholder'=>'Password Confirmation']) !!}
+                            {!! Form::password('password_confirmation', ['class' => 'form-control', 'placeholder'=>'Password Confirmation']) !!}
                         </div>
 						<div class="form-group">
 							<div id="error"></div>
