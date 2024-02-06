62bfc23e0bb0f873b5d90ea35c755f02c153f410
CF <pclienth@hotmail.com>
2024-02-06 01:17:31 +0000

    Bug Fixing

--- app/Mail/CompanyEmailVerification.php
+++ app/Mail/CompanyEmailVerification.php
@@ -44,14 +44,10 @@ class CompanyEmailVerification extends Mailable
             $body = $emailTemplate->body;
             $body = str_replace('##APP_NAME', $this->user->name, $body);
 
-            if($this->user->is_admin){
-                $company = \App\Models\Company::where('is_default', 1)->first(['logo']);
-                $body = str_replace('##APP_LOGO', asset('storage/uploads/logo/'. $company->logo), $body);
-                // $body = str_replace('##LINK', $this->user->company->v2_domain_link.'/admin/password/reset/'.$this->token, $body);
-            }else{
-                $body = str_replace('##APP_LOGO', asset('storage/uploads/logo/'. $this->user->company->logo), $body);
-                // $body = str_replace('##LINK', $this->user->company->v2_domain_link.'/password/reset/'.$this->token, $body);
-            }
+            $company = \App\Models\Company::where('is_default', 1)->first(['logo']);
+            $body = str_replace('##APP_LOGO', env('AZURE_STORAGE_URL').'uploads/'.$company->logo, $body);
+            // $body = str_replace('##LINK', $this->user->company->v2_domain_link.'/admin/password/reset/'.$this->token, $body);
+
             $body = str_replace('##USER_NAME', $this->user->full_name, $body);
 
             $this->subject($subject)
